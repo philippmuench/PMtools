467f4d19803ec3d0d10fd954c1ef6552e0cd27c2
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-06-19 18:09:22 +0200

    minor changes

--- R/data.R
+++ R/data.R
@@ -1,11 +1,12 @@
 #' Public metadata of HMP1-II study
 #' Provides sample information and body sites
+#' Can be loaded to workspace via data(hmp1_2_metadata)
 #' @format matrix
 #' @references \url{http://github.com/philippmuench}
 "hmp1_2_metadata"
 
 #' HMANn2 output of Cas gene abundnace amoung HMP1-II samples
-#' Can be loaded to workspace via data("humann2_table")
+#' Can be loaded to workspace via data(humann2_table)
 #' @format data.frame
 #' @references \url{http://github.com/philippmuench}
 "humann2_table"
