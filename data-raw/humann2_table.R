1124f999385c0b9e361be8bfc4f0191eccbd1f44
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-06-19 13:31:22 +0200

    add data and setup tests

---
+++ data-raw/humann2_table.R
@@ -0,0 +1,3 @@
+library(usethis)
+humann2_table <- as.data.frame(read.table("data-raw/humann2_table.tsv", header = T, sep = '\t', stringsAsFactors = F))
+use_data(humann2_table, overwrite = TRUE)
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(PMtools)
+
+test_check("PMtools")
