1a3dda1d0d9f7df6e5cf94231b974461c7ce3862
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-06-19 17:16:16 +0200

    add dataset, update DESC

--- data-raw/hmp1_2_metadata.R
+++ data-raw/hmp1_2_metadata.R
@@ -1,7 +1,5 @@
-library(usethis)
-
 hmp1_2_metadata <- read.csv2("data-raw/hmp1_2_metadata.tsv",
                           sep = "\t", header = T)
 hmp1_2_metadata <- hmp1_2_metadata[which(hmp1_2_metadata$SRS != "#N/A"),]
 
-use_data(hmp1_2_metadata, overwrite = TRUE)
+usethis::use_data(hmp1_2_metadata, overwrite = TRUE)
--- data-raw/humann2_table.R
+++ data-raw/humann2_table.R
@@ -1,3 +1,2 @@
-library(usethis)
 humann2_table <- as.data.frame(read.table("data-raw/humann2_table.tsv", header = T, sep = '\t', stringsAsFactors = F))
-use_data(humann2_table, overwrite = TRUE)
+usethis::use_data(humann2_table, overwrite = TRUE)
