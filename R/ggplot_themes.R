27a3289c5f4d0e724f3db749b2d34e5e134a8b67
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-06-25 22:03:15 +0200

    plot aesthetics

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -1,26 +1,27 @@
 #' Custom ggplto2 theme
 #' @export
-themePM <- function(base_size = 11, base_family = "")
+themePM <- function(base.size = 11, axis.family = "", base.family = "")
 {
-  half_line <- base_size/2
+  half_line <- base.size/2
+  line.size = 0.2
   ggplot2::theme(
-    line = ggplot2::element_line(colour = "black", size = 0.5,
+    line = ggplot2::element_line(colour = "black", size = line.size,
                         linetype = 1, lineend = "butt"),
     rect = ggplot2::element_rect(fill = "white", colour = "black",
                         size = 0.5, linetype = 1),
-    text = ggplot2::element_text(family = base_family, face = "plain",
-                        colour = "black", size = base_size,
+    text = ggplot2::element_text(family = base.family, face = "plain",
+                        colour = "black", size = base.size,
                         lineheight = 0.9,  hjust = 0.5,
                         vjust = 0.5, angle = 0,
                         margin = ggplot2::margin(), debug = FALSE),
 
-    axis.line = ggplot2::element_line(colour = "black", size = 0.5),
-    axis.text = ggplot2::element_text(size = ggplot2::rel(0.8), colour = "black"),
+    axis.line = ggplot2::element_line(colour = "black", size = line.size, linetype = 1),
+    axis.text = ggplot2::element_text(family = axis.family, size = ggplot2::rel(0.8), colour = "black"),
     axis.text.x = ggplot2::element_text(margin = ggplot2::margin(t = 0.8*half_line/2),
                                vjust = 1),
     axis.text.y = ggplot2::element_text(margin = ggplot2::margin(r = 0.8*half_line/2),
                                hjust = 1),
-    axis.ticks = ggplot2::element_line(colour = "black"),
+    axis.ticks = ggplot2::element_line(colour = "black", size = line.size),
     axis.ticks.length = ggplot2::unit(half_line/2, "pt"),
     axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 0.8 * half_line,
                                                 b = 0.8 * half_line/2)),
@@ -36,7 +37,7 @@ themePM <- function(base_size = 11, base_family = "")
     legend.key.width = NULL,
     legend.text = ggplot2::element_text(size = ggplot2::rel(0.8)),
     legend.text.align = NULL,
-    legend.title = ggplot2::element_text(hjust = 0),
+    legend.title = ggplot2::element_text(hjust = 0, size = base.size),
     legend.title.align = NULL,
     legend.position = "right",
     legend.direction = NULL,
@@ -50,11 +51,11 @@ themePM <- function(base_size = 11, base_family = "")
     panel.spacing = ggplot2::unit(half_line, "pt"), panel.margin.x = NULL,
     panel.margin.y = NULL, panel.ontop = FALSE,
 
-    strip.background = ggplot2::element_rect(colour="#f0f0f0",fill="#f0f0f0"),
-    strip.text = ggplot2::element_text(colour = "black", size = ggplot2::rel(0.8)),
-    strip.text.x = ggplot2::element_text(margin = ggplot2::margin(t = half_line,
+    strip.background = ggplot2::element_rect(colour = "#f0f0f0",fill = "#f0f0f0"),
+    strip.text = ggplot2::element_text(family = axis.family, colour = "black", size = ggplot2::rel(0.8)),
+    strip.text.x = ggplot2::element_text(size = base.size, family = axis.family, margin = ggplot2::margin(t = half_line,
                                                 b = half_line)),
-    strip.text.y = ggplot2::element_text(angle = -90,
+    strip.text.y = ggplot2::element_text(size = base.size, family = axis.family, angle = -90,
                                 margin = ggplot2::margin(l = half_line,
                                                 r = half_line)),
     strip.switch.pad.grid = ggplot2::unit(0.1, "cm"),
