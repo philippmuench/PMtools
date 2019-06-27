592c032414ac2970ac21bfbff9827923c8d61ba7
Philipp Münch <philipp.muench@helmholtz-hzi.de>
2019-06-28 00:34:10 +0200

    add metaphlan2 data

--- R/data.R
+++ R/data.R
@@ -5,8 +5,14 @@
 #' @references \url{http://github.com/philippmuench}
 "hmp1_2_metadata"
 
-#' HMANn2 output of Cas gene abundnace amoung HMP1-II samples
+#' HUMANn2 output of Cas gene abundnace amoung HMP1-II samples
 #' Can be loaded to workspace via data(humann2_table)
 #' @format data.frame
 #' @references \url{http://github.com/philippmuench}
 "humann2_table"
+
+#' Metaphlan2 HMP1-II
+#' Can be loaded to workspace via data(hmp1_2_metaphlan)
+#' @format data.frame
+#' @references \url{http://github.com/philippmuench}
+"hmp1_2_metaphlan"
